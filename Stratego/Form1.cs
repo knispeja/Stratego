c7db01f32e766d247a231d9eb7fcfe2caa45194c
Alec Tiefenthal <alectief@gmail.com>
2015-03-26 20:54:50 -0400

    Added Visual Studio Files

---
+++ Stratego/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Stratego
+{
+    public partial class StrategoWin : Form
+    {
+        ArrayList Piece { get; set; }
+        public StrategoWin()
+        {
+            InitializeComponent();
+        }
+    }
+}

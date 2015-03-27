4d4ece4cce3dfc0f13a71ce33181bd0e6d769b8d
Jacob Knispel <knispeja@rose-hulman.edu>
2015-03-27 15:53:14 -0400

    Fixed a couple small things
    - Changed name of main form
    - Made the Start button the AcceptButton (can be pressed using the Enter
    key without being selected)
    - Can no longer select the Start button with Tab
    - Can't maximize the screen anymore

---
+++ Stratego/Main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Media;
+
+namespace Stratego
+{
+    public partial class StrategoWin : Form
+    {
+        int ticks = 0;
+        ArrayList Piece { get; set; }
+        public StrategoWin()
+        {
+            InitializeComponent();
+            SoundPlayer sound = new SoundPlayer(Properties.Resources.BattleDramatic);
+            sound.PlayLooping();
+            this.StartButton.FlatStyle = FlatStyle.Flat;
+            this.StartButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, Color.Red);
+            this.StartButton.FlatAppearance.BorderSize = 0;
+            this.StartButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, Color.Red);
+            Timer t = new Timer();
+            t.Start();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sound = new SoundPlayer(Properties.Resources.no);
+            sound.Play();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void startTimer_Tick(object sender, EventArgs e)
+        {
+            ticks++;
+            if (ticks == 25)
+            {
+                this.StartButton.Visible = true;
+                this.TitlePictureBox.Visible = true;
+            }
+            else if (ticks == 40)
+            {
+                this.TitlePictureBox.Parent = this.FireBox;
+                this.StartButton.Parent = this.FireBox;
+                this.FormBorderStyle = FormBorderStyle.FixedSingle;
+                this.FireBox.Visible = true;
+                this.startTimer.Dispose();
+            }
+        }
+
+        private void StartButton_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.StartButton.Image = Properties.Resources.StartButtonClick;
+        }
+
+        private void StartButton_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.StartButton.Image = Properties.Resources.StartButton;
+        }
+
+        private void StrategoWin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

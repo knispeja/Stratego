7c897f61989b447aef60ce0ac474189937d2445e
erexsolr <erexsolr@rose-hulman.edu>
2015-04-03 01:30:37 -0400

    Added more test cases for placePiece
    Confirming test works for all remaining piece inputs.

--- StrategoTest/MainTest.cs
+++ StrategoTest/MainTest.cs
@@ -45,11 +45,31 @@ namespace StrategoTest
         [TestCase(2, 100, 100)]
         [TestCase(3, 100, 100)]
         [TestCase(4, 100, 100)]
+        [TestCase(5, 100, 100)]
+        [TestCase(6, 100, 100)]
+        [TestCase(7, 100, 100)]
+        [TestCase(8, 100, 100)]
+        [TestCase(9, 100, 100)]
+        [TestCase(10, 100, 100)]
+        [TestCase(11, 100, 100)]
+        [TestCase(12, 100, 100)]
+        [TestCase(-1, 100, 100)]
+        [TestCase(-2, 100, 100)]
+        [TestCase(-3, 100, 100)]
+        [TestCase(-4, 100, 100)]
+        [TestCase(-5, 100, 100)]
+        [TestCase(-6, 100, 100)]
+        [TestCase(-7, 100, 100)]
+        [TestCase(-8, 100, 100)]
+        [TestCase(-9, 100, 100)]
+        [TestCase(-10, 100, 100)]
+        [TestCase(-11, 100, 100)]
+        [TestCase(-12, 100, 100)]
         public void TestThatPieceIsPlacedIntoEmptySpace(int piece, int x, int y)
         {
             StrategoWin game = new StrategoWin(1000, 1000, new int[10, 10]);
             bool? result = game.placePiece(piece, x, y);
-            Assert.IsTrue(game.getPiece(x/100, y/100)==piece);
+            Assert.AreEqual(game.getPiece(x/100, y/100),piece);
             Assert.IsTrue(result.Value); 
         }
 
@@ -57,7 +77,26 @@ namespace StrategoTest
         [TestCase(2, 100, 100, Result = false)]
         [TestCase(3, 100, 100, Result = false)]
         [TestCase(4, 100, 100, Result = false)]
-       
+        [TestCase(5, 100, 100, Result = false)]
+        [TestCase(6, 100, 100, Result = false)]
+        [TestCase(7, 100, 100, Result = false)]
+        [TestCase(8, 100, 100, Result = false)]
+        [TestCase(9, 100, 100, Result = false)]
+        [TestCase(10, 100, 100, Result = false)]
+        [TestCase(11, 100, 100, Result = false)]
+        [TestCase(12, 100, 100, Result = false)]
+        [TestCase(-1, 100, 100, Result = false)]
+        [TestCase(-2, 100, 100, Result = false)]
+        [TestCase(-3, 100, 100, Result = false)]
+        [TestCase(-4, 100, 100, Result = false)]
+        [TestCase(-5, 100, 100, Result = false)]
+        [TestCase(-6, 100, 100, Result = false)]
+        [TestCase(-7, 100, 100, Result = false)]
+        [TestCase(-8, 100, 100, Result = false)]
+        [TestCase(-9, 100, 100, Result = false)]
+        [TestCase(-10, 100, 100, Result = false)]
+        [TestCase(-11, 100, 100, Result = false)]
+        [TestCase(-12, 100, 100, Result = false)]
         public bool? TestThatNothingCanBePlacedInFilledSpace(int piece, int x, int y)
         {
             int[,] map = new int[10, 10];

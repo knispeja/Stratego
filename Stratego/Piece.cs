4c2d9997728b218c9fdb4faabc64e51153f4982a
Alec Tiefenthal <alectief@gmail.com>
2015-04-02 20:44:46 -0400

    Literally nothing

--- Stratego/Piece.cs
+++ Stratego/Piece.cs
@@ -4,9 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * 0 - Blank
+ * 1-9 Normal Peieces
+ * 10 - Spy
+ * 11 - Bomb
+ * 12 - Flag
+ * NULL = place you cant move (lakes)
+ */
 namespace Stratego
 {
     class Piece
     {
+        public static int? attack(int? first, int? second)
+        {
+            return null;
+        }
     }
 }

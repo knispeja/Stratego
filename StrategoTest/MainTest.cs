eb21e2aeb834a514e64d2aca1782c707a4895a09
erexsolr <erexsolr@rose-hulman.edu>
2015-04-02 22:39:49 -0400

    Added Tests For placePiece

--- StrategoTest/MainTest.cs
+++ StrategoTest/MainTest.cs
@@ -8,5 +8,34 @@ namespace StrategoTest
     class MainTest
     {
 
+        [TestCase(1, 100, 100)]
+        public bool TestThatNothingCanBePlacedInNullSpace(int piece, int x, int y)
+        {
+            StrategoWin game = new StrategoWin(100, new int[10, 10]);
+            return game.placePiece(piece, x, y);
+        }
+
+        [TestCase(1, 100, 100)]
+        public bool TestThatPieceIsPlacedIntoEmptySpace(int piece, int x, int y)
+        {
+            StrategoWin game = new StrategoWin(100, new int[10, 10] {{0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
+                                                                     {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
+                                                                     {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
+                                                                     {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
+                                                                     {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0}});
+            return game.placePiece(piece, x, y);
+        }
+
+        [TestCase(1, 100, 100)]
+        public bool TestThatNothingCanBePlacedInFilledSpace(int piece, int x, int y)
+        {
+            StrategoWin game = new StrategoWin(100, new int[10, 10] {{1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
+                                                                     {1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
+                                                                     {1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
+                                                                     {1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
+                                                                     {1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1}});
+            return game.placePiece(piece, x, y);
+        }
+
     }
 }

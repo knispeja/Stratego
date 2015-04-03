85bdcec04165ee5b24adfab85061b17a506356e6
erexsolr <erexsolr@rose-hulman.edu>
2015-04-03 01:04:17 -0400

    Changed Tests to account for change in constructer
    Test constructer for StrategoWin uses height and width instead of scale
    now so we adjust for that in tests

--- StrategoTest/MainTest.cs
+++ StrategoTest/MainTest.cs
@@ -8,18 +8,17 @@ namespace StrategoTest
     [TestFixture()]
     class MainTest
     {
-
         [TestCase(1, 100, 100, Result = false)]
         public bool? TestThatNothingCanBePlacedInNullSpace(int piece, int x, int y)
         {
-            StrategoWin game = new StrategoWin(100, new int[10, 10]);
+            StrategoWin game = new StrategoWin(1000, 1000, new int[10, 10]);
             return game.placePiece(piece, x, y);
         }
 
         [TestCase(1, 100, 100)]
         public void TestThatPieceIsPlacedIntoEmptySpace(int piece, int x, int y)
         {
-            StrategoWin game = new StrategoWin(100, new int[10, 10] {{0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
+            StrategoWin game = new StrategoWin(1000, 1000, new int[10, 10] {{0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
                                                                      {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
                                                                      {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
                                                                      {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
@@ -32,7 +31,7 @@ namespace StrategoTest
         [TestCase(1, 100, 100, Result = false)]
         public bool? TestThatNothingCanBePlacedInFilledSpace(int piece, int x, int y)
         {
-            StrategoWin game = new StrategoWin(100, new int[10, 10] {{1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
+            StrategoWin game = new StrategoWin(1000,1000, new int[10, 10] {{1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
                                                                      {1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
                                                                      {1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
                                                                      {1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},

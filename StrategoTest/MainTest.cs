1dec165a47aa7799b16796e8ebfae6d6f69c2508
erexsolr <erexsolr@rose-hulman.edu>
2015-04-03 01:16:02 -0400

    Adjusting to change to obstacle from Null to 42

--- StrategoTest/MainTest.cs
+++ StrategoTest/MainTest.cs
@@ -9,20 +9,18 @@ namespace StrategoTest
     class MainTest
     {
         [TestCase(1, 100, 100, Result = false)]
-        public bool? TestThatNothingCanBePlacedInNullSpace(int piece, int x, int y)
         {
-            StrategoWin game = new StrategoWin(1000, 1000, new int[10, 10]);
+            int[,] map = new int[10, 10];
+            map[x/100, y/100] = 42;
+
+            StrategoWin game = new StrategoWin(1000, 1000, map);
             return game.placePiece(piece, x, y);
         }
 
         [TestCase(1, 100, 100)]
         public void TestThatPieceIsPlacedIntoEmptySpace(int piece, int x, int y)
         {
-            StrategoWin game = new StrategoWin(1000, 1000, new int[10, 10] {{0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
-                                                                     {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
-                                                                     {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
-                                                                     {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
-                                                                     {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0}});
+            StrategoWin game = new StrategoWin(1000, 1000, new int[10, 10]);
             bool? result = game.placePiece(piece, x, y);
             Assert.IsTrue(game.getPiece(x/100, y/100)==piece);
             Assert.IsTrue(result.Value); 
@@ -31,11 +29,10 @@ namespace StrategoTest
         [TestCase(1, 100, 100, Result = false)]
         public bool? TestThatNothingCanBePlacedInFilledSpace(int piece, int x, int y)
         {
-            StrategoWin game = new StrategoWin(1000,1000, new int[10, 10] {{1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
-                                                                     {1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
-                                                                     {1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
-                                                                     {1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
-                                                                     {1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1}});
+            int[,] map = new int[10, 10];
+            map[x / 100, y / 100] = 1;
+
+            StrategoWin game = new StrategoWin(1000, 1000, map);
             return game.placePiece(piece, x, y);
         }
 

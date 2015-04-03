5f3bbbefa25b7dfa385a46b81ddfa4c0491b4c31
erexsolr <erexsolr@rose-hulman.edu>
2015-04-02 22:58:55 -0400

    Fixed TestThatPieceIsPlacedIntoEmptySpace()

--- StrategoTest/MainTest.cs
+++ StrategoTest/MainTest.cs
@@ -8,26 +8,28 @@ namespace StrategoTest
     class MainTest
     {
 
-        [TestCase(1, 100, 100)]
-        public bool TestThatNothingCanBePlacedInNullSpace(int piece, int x, int y)
+        [TestCase(1, 100, 100, Result = false)]
+        public bool? TestThatNothingCanBePlacedInNullSpace(int piece, int x, int y)
         {
             StrategoWin game = new StrategoWin(100, new int[10, 10]);
             return game.placePiece(piece, x, y);
         }
 
         [TestCase(1, 100, 100)]
-        public bool TestThatPieceIsPlacedIntoEmptySpace(int piece, int x, int y)
+        public bool? TestThatPieceIsPlacedIntoEmptySpace(int piece, int x, int y)
         {
             StrategoWin game = new StrategoWin(100, new int[10, 10] {{0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
                                                                      {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
                                                                      {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
                                                                      {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
                                                                      {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0}});
-            return game.placePiece(piece, x, y);
+            bool? result = game.placePiece(piece, x, y);
+            Assert.IsTrue(game.getPiece(x/100, y/100)==piece);
+            Assert.IsTrue(result==true); 
         }
 
-        [TestCase(1, 100, 100)]
-        public bool TestThatNothingCanBePlacedInFilledSpace(int piece, int x, int y)
+        [TestCase(1, 100, 100, Result = false)]
+        public bool? TestThatNothingCanBePlacedInFilledSpace(int piece, int x, int y)
         {
             StrategoWin game = new StrategoWin(100, new int[10, 10] {{1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},
                                                                      {1,1,1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1,1,1},

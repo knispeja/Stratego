41eb18fd207a7bdfd5d3446a674bf00e90db5e8b
erexsolr <erexsolr@rose-hulman.edu>
2015-04-03 14:59:07 -0400

    Added Comments to MainTest

--- StrategoTest/MainTest.cs
+++ StrategoTest/MainTest.cs
@@ -8,9 +8,9 @@ namespace StrategoTest
     [TestFixture()]
     class MainTest
     {
-        [TestCase(1, 100, 100, Result = false)]
-        [TestCase(2, 100, 100, Result = false)]
-        [TestCase(3, 100, 100, Result = false)]
+        [TestCase(1, 100, 100, Result = false)] //Here we test that the piece value does not 
+        [TestCase(2, 100, 100, Result = false)] // affect whether it will be placed.
+        [TestCase(3, 100, 100, Result = false)] //Note that these cases are all at the top-left corner of squares
         [TestCase(4, 100, 100, Result = false)]
         [TestCase(5, 100, 100, Result = false)]
         [TestCase(6, 100, 100, Result = false)]
@@ -31,14 +31,17 @@ namespace StrategoTest
         [TestCase(-9, 100, 100, Result = false)]
         [TestCase(-10, 100, 100, Result = false)]
         [TestCase(-11, 100, 100, Result = false)]
-        [TestCase(-12, 100, 100, Result = false)]
-        [TestCase(-12, 200, 200, Result = false)]
+
+        [TestCase(-12, 100, 100, Result = false)] //Here we test that the function acts appropriately 
+        [TestCase(-12, 200, 200, Result = false)] // for placing in any diagonal space.
         [TestCase(-12, 300, 300, Result = false)]
         [TestCase(-12, 400, 400, Result = false)]
-        [TestCase(-12, 101, 101, Result = false)]
-        [TestCase(-12, 201, 201, Result = false)]
+
+        [TestCase(-12, 101, 101, Result = false)] // Here we test that the function recognizes the  
+        [TestCase(-12, 201, 201, Result = false)] // correct square for coordinates not on the top-left corner.
         [TestCase(-12, 301, 301, Result = false)]
         [TestCase(-12, 401, 401, Result = false)]
+        // This tests that pieces will not be placed on an obstacle space on a 1000 x 1000 pixel board.
         public bool? TestThatNothingCanBePlacedOnObstacle(int piece, int x, int y)
         {
             int[,] map = new int[10, 10];
@@ -48,9 +51,9 @@ namespace StrategoTest
             return game.placePiece(piece, x, y);
         }
 
-        [TestCase(1, 100, 100)]
-        [TestCase(2, 100, 100)]
-        [TestCase(3, 100, 100)]
+        [TestCase(1, 100, 100)]//Here we test that the piece value does not 
+        [TestCase(2, 100, 100)] // affect whether it will be placed.
+        [TestCase(3, 100, 100)] //Note that these cases are all at the top-left corner of squares
         [TestCase(4, 100, 100)]
         [TestCase(5, 100, 100)]
         [TestCase(6, 100, 100)]
@@ -71,14 +74,17 @@ namespace StrategoTest
         [TestCase(-9, 100, 100)]
         [TestCase(-10, 100, 100)]
         [TestCase(-11, 100, 100)]
-        [TestCase(-12, 100, 100)]
-        [TestCase(-12, 200, 200)]
+
+        [TestCase(-12, 100, 100)]//Here we test that the function acts appropriately 
+        [TestCase(-12, 200, 200)]// for placing in any diagonal space.
         [TestCase(-12, 300, 300)]
         [TestCase(-12, 400, 400)]
-        [TestCase(-12, 101, 101)]
-        [TestCase(-12, 201, 201)]
+
+        [TestCase(-12, 101, 101)]// Here we test that the function recognizes the  
+        [TestCase(-12, 201, 201)] // correct square for coordinates not on the top-left corner.
         [TestCase(-12, 301, 301)]
         [TestCase(-12, 401, 401)]
+        // This tests that pieces will be placed into an empty space on a 1000 x 1000 pixel board.
         public void TestThatPieceIsPlacedIntoEmptySpace(int piece, int x, int y)
         {
             StrategoWin game = new StrategoWin(1000, 1000, new int[10, 10]);
@@ -87,9 +93,9 @@ namespace StrategoTest
             Assert.IsTrue(result.Value); 
         }
 
-        [TestCase(1, 100, 100, Result = false)]
-        [TestCase(2, 100, 100, Result = false)]
-        [TestCase(3, 100, 100, Result = false)]
+        [TestCase(1, 100, 100, Result = false)]//Here we test that the piece value does not 
+        [TestCase(2, 100, 100, Result = false)] // affect whether it will be placed.
+        [TestCase(3, 100, 100, Result = false)] //Note that these cases are all at the top-left corner of squares
         [TestCase(4, 100, 100, Result = false)]
         [TestCase(5, 100, 100, Result = false)]
         [TestCase(6, 100, 100, Result = false)]
@@ -110,14 +116,17 @@ namespace StrategoTest
         [TestCase(-9, 100, 100, Result = false)]
         [TestCase(-10, 100, 100, Result = false)]
         [TestCase(-11, 100, 100, Result = false)]
-        [TestCase(-12, 100, 100, Result = false)]
-        [TestCase(-12, 200, 200, Result = false)]
+
+        [TestCase(-12, 100, 100, Result = false)] //Here we test that the function acts appropriately 
+        [TestCase(-12, 200, 200, Result = false)] // for placing in any diagonal space.
         [TestCase(-12, 300, 300, Result = false)]
         [TestCase(-12, 400, 400, Result = false)]
-        [TestCase(-12, 101, 101, Result = false)]
-        [TestCase(-12, 201, 201, Result = false)]
+
+        [TestCase(-12, 101, 101, Result = false)] // Here we test that the function recognizes the  
+        [TestCase(-12, 201, 201, Result = false)] // correct square for coordinates not on the top-left corner.
         [TestCase(-12, 301, 301, Result = false)]
         [TestCase(-12, 401, 401, Result = false)]
+        // This tests that pieces will not be placed on an occupied space on a 1000 x 1000 pixel board.
         public bool? TestThatNothingCanBePlacedInFilledSpace(int piece, int x, int y)
         {
             int[,] map = new int[10, 10];
@@ -127,26 +136,31 @@ namespace StrategoTest
             return game.placePiece(piece, x, y);
         }
 
-        [TestCase(1, 200, 200, Result = false)]
-        [TestCase(1, 400, 400, Result = false)]
+        [TestCase(1, 200, 200, Result = false)] //Here we test that the function acts appropriately
+        [TestCase(1, 400, 400, Result = false)] // for placing in any diagonal space.
         [TestCase(1, 600, 600, Result = false)]
         [TestCase(1, 800, 800, Result = false)]
-        [TestCase(1, 201, 201, Result = false)]
-        [TestCase(1, 401, 401, Result = false)]
+
+        [TestCase(1, 201, 201, Result = false)] // Here we test that the function recognizes the
+        [TestCase(1, 401, 401, Result = false)] // correct square for coordinates not on the top-left corner
         [TestCase(1, 601, 601, Result = false)]
         [TestCase(1, 801, 801, Result = false)]
-        [TestCase(1, 200, 201, Result = false)]
+
+        [TestCase(1, 200, 201, Result = false)] //Coordinates on top edge of square
         [TestCase(1, 400, 401, Result = false)]
         [TestCase(1, 600, 601, Result = false)]
         [TestCase(1, 800, 801, Result = false)]
-        [TestCase(1, 201, 200, Result = false)]
+
+        [TestCase(1, 201, 200, Result = false)] //Coordinates on left edge of square
         [TestCase(1, 401, 400, Result = false)]
         [TestCase(1, 601, 600, Result = false)]
         [TestCase(1, 801, 800, Result = false)]
-        [TestCase(1, 0, 201, Result = false)]
+
+        [TestCase(1, 0, 201, Result = false)] //Left edge of grid
         [TestCase(1, 0, 401, Result = false)]
         [TestCase(1, 0, 601, Result = false)]
         [TestCase(1, 0, 801, Result = false)]
+        // This tests that pieces will not be placed on an obstacle space on a 2000 x 2000 pixel board.
         public bool? TestThatNothingCanBePlacedOnObstacleV2(int piece, int x, int y)
         {
             int[,] map = new int[10, 10];
@@ -156,26 +170,31 @@ namespace StrategoTest
             return game.placePiece(piece, x, y);
         }
 
-        [TestCase(1, 200, 200, Result = false)]
-        [TestCase(1, 400, 400, Result = false)]
+        [TestCase(1, 200, 200, Result = false)] //Here we test that the function acts appropriately
+        [TestCase(1, 400, 400, Result = false)] // for placing in any diagonal space.
         [TestCase(1, 600, 600, Result = false)]
         [TestCase(1, 800, 800, Result = false)]
-        [TestCase(1, 201, 201, Result = false)]
-        [TestCase(1, 401, 401, Result = false)]
+
+        [TestCase(1, 201, 201, Result = false)]// Here we test that the function recognizes the
+        [TestCase(1, 401, 401, Result = false)] // correct square for coordinates not on the top-left corner
         [TestCase(1, 601, 601, Result = false)]
         [TestCase(1, 801, 801, Result = false)]
-        [TestCase(1, 200, 201, Result = false)]
+
+        [TestCase(1, 200, 201, Result = false)]//Coordinates on top edge of square
         [TestCase(1, 400, 401, Result = false)]
         [TestCase(1, 600, 601, Result = false)]
         [TestCase(1, 800, 801, Result = false)]
-        [TestCase(1, 201, 200, Result = false)]
+
+        [TestCase(1, 201, 200, Result = false)]//Coordinates on left edge of square
         [TestCase(1, 401, 400, Result = false)]
         [TestCase(1, 601, 600, Result = false)]
         [TestCase(1, 801, 800, Result = false)]
-        [TestCase(1, 0, 201, Result = false)]
+
+        [TestCase(1, 0, 201, Result = false)] //Left edge of grid
         [TestCase(1, 0, 401, Result = false)]
         [TestCase(1, 0, 601, Result = false)]
         [TestCase(1, 0, 801, Result = false)]
+        // This tests that pieces will not be placed on an occupied space on a 2000 x 2000 pixel board.
         public bool? TestThatNothingCanBePlacedInFilledSpaceV2(int piece, int x, int y)
         {
             int[,] map = new int[10, 10];
@@ -185,26 +204,31 @@ namespace StrategoTest
             return game.placePiece(piece, x, y);
         }
 
-        [TestCase(1, 200, 200)]
-        [TestCase(1, 400, 400)]
+        [TestCase(1, 200, 200)]//Here we test that the function acts appropriately
+        [TestCase(1, 400, 400)] // for placing in any diagonal space.
         [TestCase(1, 600, 600)]
         [TestCase(1, 800, 800)]
-        [TestCase(1, 201, 201)]
-        [TestCase(1, 401, 401)]
+
+        [TestCase(1, 201, 201)]// Here we test that the function recognizes the
+        [TestCase(1, 401, 401)] // correct square for coordinates not on the top-left corner
         [TestCase(1, 601, 601)]
         [TestCase(1, 801, 801)]
-        [TestCase(1, 200, 201)]
+
+        [TestCase(1, 200, 201)]//Coordinates on top edge of square
         [TestCase(1, 400, 401)]
         [TestCase(1, 600, 601)]
         [TestCase(1, 800, 801)]
-        [TestCase(1, 201, 200)]
+
+        [TestCase(1, 201, 200)]//Coordinates on left edge of square
         [TestCase(1, 401, 400)]
         [TestCase(1, 601, 600)]
         [TestCase(1, 801, 800)]
-        [TestCase(1, 0, 200)]
+
+        [TestCase(1, 0, 200)] //Left edge of grid
         [TestCase(1, 0, 400)]
         [TestCase(1, 0, 600)]
         [TestCase(1, 0, 800)]
+        // This tests that pieces can be placed on an empty space on a 2000 x 2000 pixel board.
         public void TestThatPieceIsPlacedIntoEmptySpaceV2(int piece, int x, int y)
         {
             StrategoWin game = new StrategoWin(2000, 2000, new int[10, 10]);
@@ -213,11 +237,12 @@ namespace StrategoTest
             Assert.IsTrue(result.Value);
         }
 
-        [TestCase(1, 200, 100)]
+        [TestCase(1, 200, 100)] //These tests work with a non-square board grid
         [TestCase(1, 200, 200)]
         [TestCase(1, 400, 200)]
         [TestCase(1, 600, 300)]
         [TestCase(1, 0, 200)]
+        // This tests that pieces can be placed in an empty space on a 2000 x 1000 pixel board.
         public void TestThatPieceIsPlacedIntoEmptySpaceV3(int piece, int x, int y)
         {
             StrategoWin game = new StrategoWin(2000, 1000, new int[10, 10]);
@@ -226,11 +251,12 @@ namespace StrategoTest
             Assert.IsTrue(result.Value);
         }
 
-        [TestCase(1, 200, 100, Result = false)]
+        [TestCase(1, 200, 100, Result = false)] //These tests work with a non-square board grid
         [TestCase(1, 200, 200, Result = false)]
         [TestCase(1, 400, 200, Result = false)]
         [TestCase(1, 600, 300, Result = false)]
         [TestCase(1, 0, 200, Result = false)]
+        // This tests that pieces will not be placed on an occupied space on a 2000 x 1000 pixel board.
         public bool? TestThatNothingCanBePlacedInFilledSpaceV3(int piece, int x, int y)
         {
             int[,] map = new int[10, 10];
@@ -240,11 +266,12 @@ namespace StrategoTest
             return game.placePiece(piece, x, y);
         }
 
-        [TestCase(1, 200, 100, Result = false)]
+        [TestCase(1, 200, 100, Result = false)] //These tests work with a non-square board grid
         [TestCase(1, 200, 200, Result = false)]
         [TestCase(1, 400, 200, Result = false)]
         [TestCase(1, 600, 300, Result = false)]
         [TestCase(1, 0, 200, Result = false)]
+        // This tests that pieces will not be placed on an obstacle space on a 2000 x 1000 pixel board.
         public bool? TestThatNothingCanBePlacedOnObstacleV3(int piece, int x, int y)
         {
             int[,] map = new int[10, 10];
@@ -254,9 +281,10 @@ namespace StrategoTest
             return game.placePiece(piece, x, y);
         }
 
-        [TestCase(1,123,254, Result = false)]
+        [TestCase(1, 123, 254, Result = false)] //These tests work with a non-square board grid 
         [TestCase(1, 246, 508, Result = false)]
         [TestCase(1, 369, 762, Result = false)]
+        // This tests that pieces will not be placed on an obstacle space on a 1230 x 2540 pixel board.
         public bool? TestThatNothingCanBePlacedOnObstacleV4(int piece, int x, int y)
         {
             int[,] map = new int[10, 10];
@@ -266,9 +294,10 @@ namespace StrategoTest
             return game.placePiece(piece, x, y);
         }
 
-        [TestCase(1, 123, 254, Result = false)]
+        [TestCase(1, 123, 254, Result = false)] //These tests work with a non-square board grid
         [TestCase(1, 246, 508, Result = false)]
         [TestCase(1, 369, 762, Result = false)]
+        // This tests that pieces will not be placed on an occupied space on a 1230 x 2540 pixel board.
         public bool? TestThatNothingCanBePlacedInFilledSpaceV4(int piece, int x, int y)
         {
             int[,] map = new int[10, 10];
@@ -278,9 +307,10 @@ namespace StrategoTest
             return game.placePiece(piece, x, y);
         }
 
-        [TestCase(1, 123, 254)]
+        [TestCase(1, 123, 254)] //These tests work with a non-square board grid
         [TestCase(1, 246, 508)]
         [TestCase(1, 369, 762)]
+        // This tests that pieces can be placed in an empty space on a 1230 x 2540 pixel board.
         public void TestThatPieceIsPlacedIntoEmptySpaceV4(int piece, int x, int y)
         {
             StrategoWin game = new StrategoWin(1230, 2540, new int[10, 10]);

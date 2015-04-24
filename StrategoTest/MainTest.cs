d3064de138be7cd17924cb41649d16b97473d59c
Jacob Knispel <knispeja@rose-hulman.edu>
2015-04-24 13:59:02 -0400

    Fixed failing tests for fillRow()
    Accidentally wrote a faulty test!

--- StrategoTest/MainTest.cs
+++ StrategoTest/MainTest.cs
@@ -757,12 +757,13 @@ namespace StrategoTest
         [TestCase(0, 3)]
         public void TestFillRow(int value, int row)
         {
-            int[,] gameBoard;
-            if(value > 0) gameBoard = new int[5, 5];
-            else gameBoard = new int[10, 10];
+            int width;
+            if (value > 0) width = 7;
+            else width = 10;
+            int[,] gameBoard = new int[width, 10];
             StrategoWin game = new StrategoWin(1000, 1000, gameBoard);
             game.fillRow(value, row);
-            for (int x = 0; x < 10; x++) Assert.AreEqual(value, game.boardState[x, row]);
+            for (int x = 0; x < width; x++) Assert.AreEqual(value, game.boardState[x, row]);
         }
     }
 }

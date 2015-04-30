ae1cfdb9a60a3f259c9717370290e499b0c5c597
Jacob Knispel <knispeja@rose-hulman.edu>
2015-04-30 16:44:29 -0400

    Wrote more failing tests for AI
    Making sure AI's constructor throws exceptions on invalid team

--- StrategoTest/AITest.cs
+++ StrategoTest/AITest.cs
@@ -17,5 +17,21 @@ namespace StrategoTest
             AI ai = new AI(win, team);
             Assert.AreEqual(team, ai.team);
         }
+
+        [TestCase(0)]
+        [TestCase(3)]
+        [TestCase(-4)]
+        [TestCase(99)]
+        [TestCase(-11)]
+        [TestCase(32)]
+        [TestCase(4)]
+        [TestCase(-99)]
+        // Tests AI's constructor for exceptions by setting the AI's team incorrectly
+        public void TestConstructorThrowsException(int invalidTeam)
+        {
+            int[,] gameBoard = new int[10, 10];
+            StrategoWin win = new StrategoWin(1000, 1000, gameBoard);
+            Assert.Throws<ArgumentException>(() => new AI(win, invalidTeam));
+        }
     }
 }

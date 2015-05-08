ba79fe1360f58ad0d4a3052dd3d628340d6140ca
Doomerdinger <alectief@gmail.com>
2015-05-07 23:32:38 -0400

    Made the test pass.

--- Stratego/Main.cs
+++ Stratego/Main.cs
@@ -954,7 +954,7 @@ namespace Stratego
                     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0} };
-            if ((Math.Abs(boardState[Y, X]) == 11) || (Math.Abs(boardState[Y, X]) == 12))
+            if ((Math.Abs(boardState[Y, X]) == 11) || (Math.Abs(boardState[Y, X]) == 12) || (Math.Abs(boardState[Y, X]) == 42))
                 return moveArray;
             if(Math.Abs(boardState[Y, X]) == 9)
             {

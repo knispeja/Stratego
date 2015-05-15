4539e6409ad28a734312cfe8c050619b4693e6b4
Doomerdinger <alectief@gmail.com>
2015-05-15 14:16:20 -0400

    killed options Label

--- Stratego/Main.Designer.cs
+++ Stratego/Main.Designer.cs
@@ -153,16 +153,7 @@
             this.OptionsPanel.TabIndex = 9;
             this.OptionsPanel.Visible = false;
             // 
-<<<<<<< HEAD
-            // OptionsLabel
-            // 
-            this.OptionsLabel.AutoSize = true;
-            this.OptionsLabel.Location = new System.Drawing.Point(251, 42);
-            this.OptionsLabel.Name = "OptionsLabel";
-            this.OptionsLabel.Size = new System.Drawing.Size(43, 13);
-            this.OptionsLabel.TabIndex = 0;
-            this.OptionsLabel.Text = "Options";
-=======
+
             // xButton
             // 
             this.xButton.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -194,7 +185,7 @@
             this.returnToMenuButton.TabIndex = 17;
             this.returnToMenuButton.Text = "Quit to Main Menu";
             this.returnToMenuButton.UseVisualStyleBackColor = true;
->>>>>>> origin/master
+
             // 
             // SaveButton
             // 

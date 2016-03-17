316873ede5c6dec5aa4de6fb8cade252fcc31ac3
Steve Trotta <TrottaSN@rose-hulman.edu>
2016-03-17 15:20:03 -0400

    Making Temporary "Dead Sound" Stop.

--- Stratego/Main.cs
+++ Stratego/Main.cs
@@ -10,9 +10,102 @@ using System.Windows.Forms;
 using System.Collections;
 using System.Media;
 using System.IO;
+using System.Reflection;
+using WMPLib;
+using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace Stratego
 {
+
+    public static class SoundPlayerAsync
+    {
+        [DllImport("winmm.dll", SetLastError = true)]
+        public static extern bool PlaySound(byte[] ptrToSound,
+           System.UIntPtr hmod, uint fdwSound);
+
+        [DllImport("winmm.dll", SetLastError = true)]
+        public static extern bool PlaySound(IntPtr ptrToSound,
+           System.UIntPtr hmod, uint fdwSound);
+
+        static private GCHandle? gcHandle = null;
+        private static byte[] bytesToPlay = null;
+        private static byte[] BytesToPlay
+        {
+            get { return bytesToPlay; }
+            set
+            {
+                FreeHandle();
+                bytesToPlay = value;
+            }
+        }
+
+        public static void PlaySound(System.IO.Stream stream)
+        {
+            PlaySound(stream, SoundFlags.SND_MEMORY |
+                              SoundFlags.SND_ASYNC);
+        }
+
+        public static void PlaySound(System.IO.Stream stream,
+                                     SoundFlags flags)
+        {
+            LoadStream(stream);
+            flags |= SoundFlags.SND_ASYNC;
+            flags |= SoundFlags.SND_MEMORY;
+
+            if (BytesToPlay != null)
+            {
+                gcHandle = GCHandle.Alloc(BytesToPlay,
+                                         GCHandleType.Pinned);
+                PlaySound(gcHandle.Value.AddrOfPinnedObject(),
+                                     (UIntPtr)0, (uint)flags);
+            }
+            else
+            {
+                PlaySound((byte[])null, (UIntPtr)0, (uint)flags);
+            }
+        }
+
+        private static void LoadStream(System.IO.Stream stream)
+        {
+            if (stream != null)
+            {
+                byte[] bytesToPlay = new byte[stream.Length];
+                stream.Read(bytesToPlay, 0, (int)stream.Length);
+                BytesToPlay = bytesToPlay;
+            }
+            else
+            {
+                BytesToPlay = null;
+            }
+        }
+
+        private static void FreeHandle()
+        {
+            if (gcHandle != null)
+            {
+                PlaySound((byte[])null, (UIntPtr)0, (uint)0);
+                gcHandle.Value.Free();
+                gcHandle = null;
+            }
+        }
+    }
+
+    [Flags]
+    public enum SoundFlags : int
+    {
+        SND_SYNC = 0x0000,            // play synchronously (default)
+        SND_ASYNC = 0x0001,        // play asynchronously
+        SND_NODEFAULT = 0x0002,        // silence (!default) if sound not found
+        SND_MEMORY = 0x0004,        // pszSound points to a memory file
+        SND_LOOP = 0x0008,            // loop the sound until next sndPlaySound
+        SND_NOSTOP = 0x0010,        // don't stop any currently playing sound
+        SND_NOWAIT = 0x00002000,        // don't wait if the driver is busy
+        SND_ALIAS = 0x00010000,        // name is a registry alias
+        SND_ALIAS_ID = 0x00110000,        // alias is a predefined id
+        SND_FILENAME = 0x00020000,        // name is file name
+    }
+
     public partial class StrategoWin : Form
     {
         /// <summary>
@@ -139,14 +232,14 @@ namespace Stratego
         /// </summary>
         public StrategoWin()
         {
+            SoundPlayerAsync.PlaySound(Properties.Resources.BattleDramatic);
             InitializeComponent();
             this.DoubleBuffered = true;
-            SoundPlayer sound = new SoundPlayer(Properties.Resources.BattleDramatic);
-            sound.PlayLooping();
             this.StartButton.FlatStyle = FlatStyle.Flat;
             this.StartButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, Color.Red);
             this.StartButton.FlatAppearance.BorderSize = 0;
             this.StartButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, Color.Red);
+            System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
             this.panelWidth = this.backPanel.Width;
             this.panelHeight = this.backPanel.Height;
             this.turn = 0;
@@ -158,6 +251,7 @@ namespace Stratego
             this.movableFlags = false;
             this.level = -1;
             this.backPanel.LostFocus += onBackPanelLostFocus;
+            t.Start();
 
             // Initialize the board state with invalid spaces in the enemy player's side
             // of the board and empty spaces everywhere else. To be changed later!
@@ -168,6 +262,20 @@ namespace Stratego
 
             this.backPanel.Focus();
         }
+
+        /*
+
+        private static void MusicInBackground()
+        {
+            UnmanagedMemoryStream uM = Properties.Resources.BattleDramatic;
+            SoundPlayer sp = new SoundPlayer(uM);
+            while (true)
+            {
+                sp.PlaySync();
+            }
+        }
+
+        */
 
         /// <summary>
         /// Initializer for the testing framework.
@@ -307,12 +415,15 @@ namespace Stratego
         /// <param name="e"></param>
         private void StartButton_Click(object sender, EventArgs e)
         {
+            /* Make function for below action - using dead memory stream to signify clearing all music on the other thread */
+            Stream streamDead = new MemoryStream();
+            SoundPlayerAsync.PlaySound(streamDead);
+
             SoundPlayer sound = new SoundPlayer(Properties.Resources.no);
             sound.Play();
             this.FireBox.Dispose();
             this.placements = (int[])this.defaults.Clone();
             this.backPanel.BackgroundImage = Properties.Resources.BoardUpdate;
-
             nextTurn();
             this.LoadButton.Visible = false;
             this.LoadButton.Enabled = false;
@@ -356,7 +467,6 @@ namespace Stratego
             this.ticks++;
             if (this.ticks == 25)
             {
-
                 this.TitlePictureBox.Visible = true;
             }
             else if (this.ticks == 40)
@@ -375,7 +485,7 @@ namespace Stratego
                 this.CampaignButton.Enabled = true;
                 this.SinglePlayerButton.Visible = true;
                 this.SinglePlayerButton.Enabled = true;
-                this.startTimer.Dispose();
+                // this.startTimer.Dispose();
             }
         }
 

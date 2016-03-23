c9e22d2b649f824a45a7097840c2b99b80a85cb1
Steve Trotta <TrottaSN@rose-hulman.edu>
2016-03-22 22:51:25 -0400

    More PieceTesting - Came Up With Solid Test Plan

--- StrategoTest/PieceTest.cs
+++ StrategoTest/PieceTest.cs
@@ -6,9 +6,35 @@ using System.Collections.Generic;
 
 namespace StrategoTest
 {
+
+    /*
+    S v S *
+    S v D *
+    S v M
+    S v F
+    S v B
+
+    M v M
+    M v B
+    M v F
+    M v D
+
+    B v B
+    B v F
+    B v D
+
+    F v F
+    F v D
+
+    D v D
+    */
+
     [TestFixture()]
     public class PieceTest
     {
+        public static readonly int BOTH_DEAD = 99;
+        public static readonly int FAILURE = 100;
+
         //Marshal = 1, General = 2, Colonel = 3, Major = 4, Captain = 5, Lieutenant = 6, Sergeant = 7, Miner = 8, Cout = 9, Spy = 10, Bomb = 11, Flag = 12;
         public static IEnumerable<TestCaseData> SpyVSSpyData
         {
@@ -16,145 +42,49 @@ namespace StrategoTest
             {
                 GamePiece newSpy0 = new SpyPiece(0);
                 GamePiece newSpy1 = new SpyPiece(1);
-                yield return new TestCaseData(newSpy0, newSpy1, 99);
+                yield return new TestCaseData(newSpy0, newSpy1, BOTH_DEAD);
             }
         }
-        /*
-
-        [TestCase(newSpy, newSpy, 1)] //Tests for regular cases where lowest absolute
-        [TestCase(-4, 7, -4)] //value wins
-        [TestCase(9, -5, -5)]
-        [TestCase(1, -10, 1)]
-        [TestCase(8, -12, 8)]
-        [TestCase(1, -2, 1)]
-        [TestCase(1, -3, 1)]
-        [TestCase(1, -4, 1)]
-        [TestCase(1, -5, 1)]
-        [TestCase(1, -6, 1)]
-        [TestCase(1, -7, 1)]
-        [TestCase(1, -8, 1)]
-        [TestCase(1, -9, 1)]
-        [TestCase(1, -10, 1)]
-        [TestCase(1, -12, 1)]
-        [TestCase(2, -1, -1)]
-        [TestCase(2, -2, 0)]
-        [TestCase(2, -3, 2)]
-        [TestCase(2, -4, 2)]
-        [TestCase(2, -5, 2)]
-        [TestCase(2, -6, 2)]
-        [TestCase(2, -7, 2)]
-        [TestCase(2, -8, 2)]
-        [TestCase(2, -9, 2)]
-        [TestCase(2, -10, 2)]
-        [TestCase(2, -12, 2)]
-        [TestCase(3, -1, -1)]
-        [TestCase(3, -2, -2)]
-        [TestCase(3, -3, 0)]
-        [TestCase(3, -4, 3)]
-        [TestCase(3, -5, 3)]
-        [TestCase(3, -6, 3)]
-        [TestCase(3, -7, 3)]
-        [TestCase(3, -8, 3)]
-        [TestCase(3, -9, 3)]
-        [TestCase(3, -10, 3)]
-        [TestCase(3, -12, 3)]
-        [TestCase(8, -1, -1)]
-        [TestCase(8, -2, -2)]
-        [TestCase(8, -3, -3)]
-        [TestCase(8, -4, -4)]
-        [TestCase(8, -5, -5)]
-        [TestCase(8, -6, -6)]
-        [TestCase(8, -7, -7)]
-        [TestCase(8, -8, 0)]
-        [TestCase(8, -9, 8)]
-        [TestCase(8, -10, 8)]
-        [TestCase(8, -12, 8)]
-        [TestCase(-1, 2, -1)]
-        [TestCase(-1, 3, -1)]
-        [TestCase(-1, 4, -1)]
-        [TestCase(-1, 5, -1)]
-        [TestCase(-1, 6, -1)]
-        [TestCase(-1, 7, -1)]
-        [TestCase(-1, 8, -1)]
-        [TestCase(-1, 9, -1)]
-        [TestCase(-1, 10, -1)]
-        [TestCase(-1, 12, -1)]
-        [TestCase(-2, 1, 1)]
-        [TestCase(-2, 3, -2)]
-        [TestCase(-2, 4, -2)]
-        [TestCase(-2, 5, -2)]
-        [TestCase(-2, 6, -2)]
-        [TestCase(-2, 7, -2)]
-        [TestCase(-2, 8, -2)]
-        [TestCase(-2, 9, -2)]
-        [TestCase(-2, 10, -2)]
-        [TestCase(-2, 12, -2)]
-        [TestCase(-3, 1, 1)]
-        [TestCase(-3, 2, 2)]
-        [TestCase(-3, 4, -3)]
-        [TestCase(-3, 5, -3)]
-        [TestCase(-3, 6, -3)]
-        [TestCase(-3, 7, -3)]
-        [TestCase(-3, 8, -3)]
-        [TestCase(-3, 9, -3)]
-        [TestCase(-3, 10, -3)]
-        [TestCase(-3, 12, -3)]
-        [TestCase(-8, 1, 1)]
-        [TestCase(-8, 2, 2)]
-        [TestCase(-8, 3, 3)]
-        [TestCase(-8, 4, 4)]
-        [TestCase(-8, 5, 5)]
-        [TestCase(-8, 6, 6)]
-        [TestCase(-8, 7, 7)]
-        [TestCase(-8, 9, -8)]
-        [TestCase(-8, 10, -8)]
-        [TestCase(-8, 12, -8)]
-
-        [TestCase(-3, 3, 0)] // Test tie cases
-        [TestCase(-10, 10, 0)]
-        [TestCase(6, -6, 0)]
-        [TestCase(-8, 8, 0)]
-        [TestCase(-2, 2, 0)]
-        [TestCase(-1, 1, 0)]
-        [TestCase(1, -1, 0)]
-
-        [TestCase(-3, 11, 11)] // Test that bomb beats all attackers except the miner
-        [TestCase(3, -11, -11)]
-        [TestCase(-2, 11, 11)]
-        [TestCase(2, -11, -11)]
-        [TestCase(-1, 11, 11)]
-        [TestCase(1, -11, -11)]
-
-        [TestCase(8, -11, 8)] // Test that bomb is beaten by miners
-        [TestCase(-8, 11, -8)]
-
-        [TestCase(10, -1, 10)] // Test that Spy beats marshall if it attacks
-        [TestCase(-10, 1, -10)]
-
-        [TestCase(11, -11, 0)] // Test that movable bombs kill one another
-        [TestCase(-11, 11, 0)]
-        */
-        // Tests the various cases for the attack function for pieces.
+        
         [TestCaseSource("SpyVSSpyData")]
-        public void TestAttack(GamePiece a, GamePiece b, int c)
+        public void TestSpyVSpyBattle(GamePiece a, GamePiece b, int c)
         {
-            int BOTH_DEAD = 99;
+            Assert.AreEqual(c, returnExpectedOnAttack(a, b));
+            GamePiece attack2Spy = new SpyPiece(0);
+            GamePiece defend2Spy = new SpyPiece(1);
+            Assert.AreEqual(c, returnExpectedOnAttack(attack2Spy, defend2Spy));
+        }
+
+        [TestCase(1, 2)]
+        public void TestSpyVDefaultBattle(int r1, int r2)
+        {
+            GamePiece a1Spy = new SpyPiece(0);
+            GamePiece d1Scout = new ScoutPiece(1);
+            Assert.AreEqual(r1, returnExpectedOnAttack(a1Spy, d1Scout));
+            GamePiece a2Scout = new ScoutPiece(0);
+            GamePiece d2Spy = new SpyPiece(1);
+            Assert.AreEqual(r2, returnExpectedOnAttack(a2Scout, d2Spy));
+        }
+
+        public int returnExpectedOnAttack(GamePiece a, GamePiece b)
+        {
             a.attack(b);
             b.defend(a);
             if (a.isAlive())
             {
-                Assert.AreEqual(c, a.getPieceRank());
+                return a.getPieceRank();
             }
             else if (b.isAlive())
             {
-                Assert.AreEqual(c, b.getPieceRank());
+                return b.getPieceRank();
             }
-            else if (a.isAlive() && b.isAlive()){
-                Assert.IsTrue(false);
+            else if (a.isAlive() && b.isAlive())
+            {
+                return FAILURE;
             }
             else
             {
-                Assert.AreEqual(c, BOTH_DEAD);
+                return BOTH_DEAD;
             }
         }
 

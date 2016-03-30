527574c07f552dd31761b62d3ae8fc8dca3323d2
Logan Erexson <erexsolr@rose-hulman.edu>
2016-03-29 22:52:41 -0400

    Completed Obstacle Piece

---
+++ Stratego/BattleBehaviors/Impassible.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratego.BattleBehaviors
+{
+    class Impassible : BattleBehavior
+    {
+        public override bool decideFate(GamePiece affectedPiece, GamePiece affectingPiece)
+        {
+            return false;
+        }
+    }
+}
---
+++ Stratego/GamePieces/ObstaclePiece.cs
@@ -0,0 +1,24 @@
+﻿using Stratego.BattleBehaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratego
+{
+    class ObstaclePiece : GamePiece
+    {
+        public static readonly String OBSTACLE_NAME = "Obstacle";
+        public static readonly int OBSTACLE_RANK = 3;
+
+        public ObstaclePiece(int teamCode) : base(teamCode)
+        {
+            this.teamCode = 0;
+            this.pieceRank = OBSTACLE_RANK;
+            this.pieceName = OBSTACLE_NAME;
+            this.attackBehavior = new Impassible();
+            this.defendBehavior = new Impassible();
+        }
+    }
+}

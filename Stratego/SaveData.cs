78793304074d09a8d5a0027821ef535eb7bae5c2
Jacob Knispel <knispeja@rose-hulman.edu>
2016-03-30 06:03:16 -0400

    Updated SaveData and SetupData
    Will likely get rid of SaveData entirely

--- Stratego/SaveData.cs
+++ Stratego/SaveData.cs
@@ -6,17 +6,15 @@ using System.Threading.Tasks;
 
 namespace Stratego
 {
-    /// <summary>
-    /// Simple data structure to hold any save data passed to and from save/load operations
-    /// </summary>
-    public struct SaveData
+    [Serializable]
+    public class SaveData
     {
-        public GamePiece[,] boardState { get; private set; }
+        public Gameboard boardState { get; private set; }
         public int difficulty { get; private set; }
         public int turn { get; private set; }
         public bool isSinglePlayer { get; private set; }
 
-        public SaveData(GamePiece[,] boardState, int difficulty, int turn, bool isSinglePlayer) : this()
+        public SaveData(Gameboard boardState, int difficulty, int turn, bool isSinglePlayer)
         {
             this.boardState = boardState;
             this.difficulty = difficulty;

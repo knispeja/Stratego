b1369d71c124b93076f35eb9968d8e818420d1b7
lanejn <lanejn@rose-hulman.edu>
2016-05-12 19:24:54 -0400

    Removed unnecessary AI code

--- Stratego/AI/AIv2.cs
+++ Stratego/AI/AIv2.cs
@@ -1,13 +1,9 @@
 namespace Stratego
 {
-    using System;
-    using Stratego.AI;
     public class AIv2
     {
         private Gameboard board;
-        private AIGameboard internalBoard;
         private int difficulty;
-        private GameTree gameTree;
         private Move moveToMake;
 
         public AIv2(Gameboard initBoard, int difficulty)
@@ -52,45 +48,7 @@ namespace Stratego
                         }
                     }
                 }
-            }
-            /*
-            // TODO: thread this so length depends on difficulty
-            Move move = Move.NULL_MOVE;
-            for (int i = 0; i < 999999; i++)
-            {
-                runMTCS(internalBoard, this.gameTree.getRoot());
-                move = getMostPromisingMove();
-            }
-            this.gameTree.setRoot(this.gameTree.getRoot().getChildFromMove(move));
-            return move;*/
-        }
-
-        public bool runMTCS(AIGameboard internalBoard, GameNode node)
-        {
-            if (internalBoard.isGameOver())
-            {
-                node.incrementOutcomes();
-                if (internalBoard.isWin())
-                {
-                    node.incrementWins();
-                    return true;
-                }
-                return false;
-            }
-            Move move = selectMove();
-            //bool won = runMTCS(internalBoard.move(move), node.getChildFromMove(move));
-
-            return false;//return won;
-        }
-
-        private Move selectMove()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Move getMostPromisingMove()
-        {
-            return GameTree.generateMove(this.gameTree.getRoot(), this.gameTree.getMostPromisingChild());
+            }            
         }
     }
 }
